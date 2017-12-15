8e466f8b50062e755dbaa1fa3dc5943e26778a8c
DmytroRoik <dmytroroik@gmail.com>
2017-12-15 19:36:32 +0200

    fixed charts value for PSK4 and 16QAM

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,6 +1,12 @@
-      google.charts.load('current', {'packages':['corechart']});
-     // google.charts.setOnLoadCallback(drawChart);
-
+     var MODULATION = (function () {
+       var module={}
+       module.init=function () {
+         google.charts.load('current', {'packages':['corechart']});
+       }
+       return module;
+     })();
+     MODULATION.init();
+    
 
      var  PSK = {
       PSK_4: 4,
@@ -78,66 +84,82 @@
 
           var temp_code=code[i*4]+code[i*4+1]+code[i*4+2]+code[i*4+3];
           if(temp_code=='0000'){
-            data.addRows([[0.33,0.33,temp_code]]);
+            data.addRows([[0.4472,0.4472,temp_code]]);
           }
           else if(temp_code=='0001'){
-            data.addRows([[0.33,-1,temp_code]]);
+            data.addRows([[0.4472,1.3416,temp_code]]);
           }
           else if(temp_code=='0010'){
-            data.addRows([[-1,0.33,temp_code]]);
+            data.addRows([[1.3416,0.4472,temp_code]]);
           }
           else if(temp_code=='0011'){
-            data.addRows([[-1,-1,temp_code]]);
+            data.addRows([[1.3416,1.3416,temp_code]]);
           }
           else if(temp_code=='0100'){
-            data.addRows([[0.33,0.33,temp_code]]);
+            data.addRows([[0.4472,-0.4472,temp_code]]);
           }
           else if(temp_code=='0101'){
-            data.addRows([[-0.33,1,temp_code]]);
+            data.addRows([[0.4472,-1.3416,temp_code]]);
           }
           else if(temp_code=='0110'){
-            data.addRows([[-1,-0.33,temp_code]]);
+            data.addRows([[1.3416,-0.4472,temp_code]]);
           }
           else if(temp_code=='0111'){
-            data.addRows([[-1,1,temp_code]]);
+            data.addRows([[1.3416,-1.3416,temp_code]]);
           }
           else if(temp_code=='1000'){
-            data.addRows([[0.33,-0.33,temp_code]]);
+            data.addRows([[-0.4472,0.4472,temp_code]]);
           }
           else if(temp_code=='1001'){
-            data.addRows([[0.33,-1,temp_code]]);
+            data.addRows([[-0.4472,1.3416,temp_code]]);
           }
           else if(temp_code=='1010'){
-            data.addRows([[1,-0.33,temp_code]]);
+            data.addRows([[-1.3416,0.4472,temp_code]]);
           }
           else if(temp_code=='1011'){
-            data.addRows([[1,-1,temp_code]]);
+            data.addRows([[-1.3416,1.3416,temp_code]]);
           }
           else if(temp_code=='1100'){
-            data.addRows([[0.33,0.33,temp_code]]);
+            data.addRows([[-0.4472,-0.4472,temp_code]]);
           }
           else if(temp_code=='1101'){
-            data.addRows([[0.33,1,temp_code]]);
+            data.addRows([[-0.4472,-1.3416,temp_code]]);
           }
           else if(temp_code=='1110'){
-            data.addRows([[1,0.33,temp_code]]);
+            data.addRows([[-1.3416,-0.4472,temp_code]]);
           }
           else if(temp_code=='1111'){
-            data.addRows([[1,1,temp_code]]);
+            data.addRows([[-1.3416,-1.3416,temp_code]]);
           }
         }
       }
       return data;
+
     }
 
-    function Array_with_error (miss) {
+    function Array_with_error (type_PSK,code,SNR) {
+        var data = createArray(type_PSK,code);
+        //data.og -- points
+        console.log(data.og);
+        if(SNR>=30)return data;
+        if(type_PSK==PSK.PSK_4){
+          if(SNR>=16)return data;
+          else{
 
+          }
+        }
+        else if(type_PSK==PSK.PSK_16){
+          if(SNR>=25)return data;
+          else{
+
+          }
+        }
     }
 
     function drawChart() {
 
       var data = createArray(modulation,enteredCode);
-console.log(data);
+      console.log(data);
       var options = {
         title: '',
         hAxis: {title: 'I', minValue: -2, maxValue: 2},
@@ -152,7 +174,7 @@ console.log(data);
 
     function draw_chart2()
     {
-      var data = createArray(modulation,enteredCode);
+      var data = Array_with_error(modulation,enteredCode);
 
       var options = {
         title: '',
